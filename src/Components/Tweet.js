c80d48039e6655a3bfd310e4299bf41b7fbb738b
Amanda Maharjan <maharjanamanda@gmail.com>
2020-07-26 13:07:27 +0545

    tweet simple styling

--- src/Components/Tweet.js
+++ src/Components/Tweet.js
@@ -2,10 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const TweetComponent = ({ tweet }) => {
-    console.log(tweet);
     return (
         <>
             <div className="tweet-cont">
+                <div className="user-cont">
+                    <a href={`https://twitter.com/${tweet.user.screen_name}`} className="link-to-profile">
+                        <img
+                            className="display-picture"
+                            src={tweet.user.profile_image_url}
+                        />
+                        <>
+                            <span className="user-name"> {tweet.user.screen_name} </span>
+                        </>
+                    </a>
+                </div>
                 <p> {tweet["full_text"]} </p>
             </div>
         </>
